ee73da531f2800d3f3a293120de68e088af87872
kabalera82 <kabalera@gmail.com>
2026-08-30 16:36:14 +0200

    feat: cablea todas las rutas y monta el proveedor de autenticación

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,26 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Home from './pages/Home';
+import Conocenos from './pages/Conocenos';
+import Shop from './pages/Shop';
+import Account from './pages/Account';
 import NotFound from './pages/NotFound';
+import {AuthProvider} from './context/AuthContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="conocenos" element={<Conocenos />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="cuenta" element={<Account />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
